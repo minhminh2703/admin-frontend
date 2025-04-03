6caadf2d487d722e4e976ac87c76db2a1b596761
danghuynh-bit <100502182+danghuynh-bit@users.noreply.github.com>
2025-04-03 21:01:26 +0700

    Update src/api/credential.api.ts
    Co-authored-by: Minh Minh <101251123+minhminh2703@users.noreply.github.com>

--- src/api/credential.api.ts
+++ src/api/credential.api.ts
@@ -2,7 +2,7 @@ import axios, { AxiosInstance, AxiosResponse, InternalAxiosRequestConfig } from 
 
 // Create an axios instance
 const apiClient: AxiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080/api', // use environment variables for flexibility
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080/api', // use environment variables for flexibility
   timeout: 1000000, // Set a timeout limit
   headers: {
     'Content-Type': 'application/json',
