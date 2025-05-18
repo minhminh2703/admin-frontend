dcff64458088527c5367d51e8150c0cfb79f68df
Minh Minh <101251123+minhminh2703@users.noreply.github.com>
2025-05-18 15:11:00 +0700

    Create user.ts (#15)

---
+++ src/types/user.ts
@@ -0,0 +1,33 @@
+export interface User {
+    avatar: string;
+    avatar_folder: string;
+    created_at: string;
+    email: string;
+    first_name: string;
+    id: number;
+    last_name: string;
+    password: string;
+    premium: boolean;
+    role: string;
+    status: string;
+    updated_at: string;
+    username: string;
+}
+
+export interface GetUserResponse {
+    user: User;
+}
+
+export interface ListUser {
+    users: User[];
+}
+
+export interface UserUpdateData {
+    premium: boolean;
+    status: string;
+    role: string;
+}
+
+export interface AvatarResponse {
+    avatar_download_url: string;
+}
